10543454adbe57e9e108178de9d82102376ee2f9
BBaoVanC <bbaovanc@bbaovanc.com>
2024-08-16 01:50:54 -0500

    WIP cleaner

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
+use std::iter::repeat;
+
 fn main() {
     // TODO: limit to only real combinations
+
     let ssns = (000..=999)
-        .flat_map(|a| std::iter::repeat(a)
-            .map(|a| {
-                let mut s = String::with_capacity(11);
-                s.push_str(&format!("{:0>3}", a));
-                s
-            }) // create strings with enough capacity to avoid extra allocations
-            .zip(00..=99)
-            .map(|ab| format!("{}-{:0>2}", ab.0, ab.1))
-        )
-        .flat_map(|b| std::iter::repeat(b)
-            .zip(0000..=9999)
-            .map(|bc| format!("{}-{:0>4}", bc.0, bc.1))
-        );
-    
-    // example: grab the first 50
+        .flat_map(|a| repeat(a).zip(00..=99))
+        .flat_map(|ab| repeat(ab).zip(0000..=9999))
+        .map(|abc| format!("{:0>3}-{:0>2}-{:0>4}", abc.0.0, abc.0.1, abc.1));
+
+
     dbg!(ssns.take(50).collect::<Vec<_>>());
 }
